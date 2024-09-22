af99e14588bfc0401690d2c07856fb22dfb45a8a
nedaras <nedas.pranskunas@gmail.com>
2024-09-22 18:26:27 +0300

    Made an iterator from callback funtion looks much better.

---
+++ src/enet/enet.zig
@@ -0,0 +1,164 @@
+const std = @import("std");
+const mem = std.mem;
+const Allocator = mem.Allocator;
+
+const c = @cImport({
+    @cInclude("enet/enet.h");
+});
+
+pub const Address = struct {
+    host: [:0]const u8,
+    port: u16,
+};
+
+pub const Packet = struct {
+    type: u32, // make an enum on types
+    data: []const u8,
+};
+
+pub const Connection = struct {
+    host: *c.ENetHost,
+    peer: *c.ENetPeer,
+
+    pub fn next(connection: Connection) !?Packet {
+        var event: c.ENetEvent = undefined;
+        while (true) { // can we make it async io? it would be very nice to handle multiple connections without needing multiple threads
+            while (c.enet_host_service(connection.host, &event, 0) > 0) switch (event.type) {
+                c.ENET_EVENT_TYPE_CONNECT => {},
+                c.ENET_EVENT_TYPE_NONE => {},
+                c.ENET_EVENT_TYPE_RECEIVE => {
+                    const slice = event.packet.*.data[0..event.packet.*.dataLength];
+                    if (slice.len < 4) return error.InvalidLength;
+
+                    return .{
+                        .type = mem.readInt(u32, slice[0..4], .little),
+                        .data = slice[4..],
+                    };
+                },
+                else => return null,
+            };
+            try std.Thread.yield();
+        }
+    }
+
+    pub fn deinit(connection: Connection) void {
+        c.enet_host_destroy(connection.host);
+        c.enet_deinitialize();
+    }
+
+    pub const SendError = error{
+        ENetPacketCreate,
+        ENetPeerSend,
+    };
+
+    pub fn sendPacket(connection: Connection, comptime packet_type: u32, comptime fmt: []const u8, args: anytype) SendError!void {
+        comptime var prefix: [4]u8 = undefined;
+        mem.writeInt(u32, &prefix, packet_type, .little);
+
+        const raw_packet = prefix ++ fmt;
+        const raw_packet_len = std.fmt.count(raw_packet, args);
+
+        if (raw_packet_len <= raw_packet.len) {
+            const packet = c.enet_packet_create(raw_packet.ptr, raw_packet_len, c.ENET_PACKET_FLAG_RELIABLE | c.ENET_PACKET_FLAG_NO_ALLOCATE);
+            if (packet == null) {
+                return SendError.ENetPacketCreate;
+            }
+            errdefer c.enet_packet_destroy(packet);
+
+            if (c.enet_peer_send(connection.peer, 0, packet) != 0) {
+                return SendError.ENetPeerSend;
+            }
+
+            return;
+        }
+
+        const packet = c.enet_packet_create(null, raw_packet_len, c.ENET_PACKET_FLAG_RELIABLE);
+        if (packet == null) {
+            return SendError.ENetPacketCreate;
+        }
+        errdefer c.enet_packet_destroy(packet);
+
+        _ = std.fmt.bufPrint(packet.*.data[0..packet.*.dataLength], raw_packet, args) catch unreachable;
+
+        if (c.enet_peer_send(connection.peer, 0, packet) != 0) {
+            return SendError.ENetPeerSend;
+        }
+    }
+};
+
+pub fn connectToServer(allocator: Allocator, address: Address) !Connection {
+    if (allocator.vtable == std.heap.c_allocator.vtable) {
+        if (c.enet_initialize() != 0) {
+            return error.EnetInitialize;
+        }
+    } else {
+        const Impl = struct {
+            var base_allocator: Allocator = undefined;
+
+            fn malloc(size: usize) callconv(.C) ?*anyopaque {
+                const header = @sizeOf(usize);
+                const alignment = @alignOf(std.c.max_align_t);
+
+                const block = base_allocator.alignedAlloc(u8, alignment, size + header) catch return null;
+                block[0..header].* = @bitCast(size);
+
+                return block.ptr + header;
+            }
+
+            fn free(memory: ?*anyopaque) callconv(.C) void {
+                const ptr = memory orelse return;
+
+                const header = @sizeOf(usize);
+                const alignment = @alignOf(std.c.max_align_t);
+
+                // this is unsafe if enet allocs data not using our allocator and frees it using ours
+                const block_ptr: *anyopaque = @ptrFromInt(@intFromPtr(ptr) - header); // we need to see asm to make sure that it only devides
+                const block: [*]align(alignment) u8 = @ptrCast(@alignCast(block_ptr));
+
+                const size: usize = @bitCast(block[0..header].*);
+                base_allocator.free(block[0 .. size + header]);
+            }
+        };
+        // is it safe on multithreaded enviroments? we should add mutixes here just for safety base_allocator is a static variable
+        Impl.base_allocator = allocator;
+
+        if (c.enet_initialize_with_callbacks(c.ENET_VERSION, &.{
+            .malloc = Impl.malloc,
+            .free = Impl.free,
+            .no_memory = null, // we do not need it
+        }) != 0) {
+            return error.EnetInitialize;
+        }
+    }
+    errdefer c.enet_deinitialize();
+
+    const client = c.enet_host_create(null, 1, 2, 0, 0);
+    if (client == null) {
+        return error.ENetHostCreate;
+    }
+    errdefer c.enet_host_destroy(client);
+
+    var enet_address: c.ENetAddress = undefined;
+
+    client.*.usingNewPacket = 1;
+    client.*.checksum = c.enet_crc32;
+
+    if (c.enet_host_compress_with_range_coder(client) != 0) {
+        return error.ENetAddressCompress;
+    }
+
+    if (c.enet_address_set_host(&enet_address, address.host) != 0) {
+        return error.ENetAddressSetHost;
+    }
+    enet_address.port = address.port;
+
+    const peer = c.enet_host_connect(client, &enet_address, 2, 0);
+    if (peer == null) {
+        return error.ENetHostConnect;
+    }
+
+    return .{
+        .host = client,
+        .peer = peer,
+    };
+}
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,5 @@
 const std = @import("std");
-const c = @cImport({
-    @cInclude("enet/enet.h");
-});
+const enet = @import("enet/enet.zig");
 
 // outdated, just for refrence
 // SendPacket IDA (Pseudocode)
@@ -31,185 +29,33 @@ const Arguments = struct {
     token: []const u8,
 };
 
-const Address = struct {
-    host: [:0]const u8,
-    port: u16,
-};
-
-const Packet = struct {
-    type: u32, // make an enum on types
-    data: []const u8,
-};
-
-fn on_packet(allocator: std.mem.Allocator, packet: Packet, sender: PacketSender) !void {
-    _ = allocator; // just make a lambda so we would not need to pass these stuff if not possible use context and comptime
-
-    std.debug.print("got a packet({d}):\n{s}\n", .{ packet.type, packet.data });
-    if (packet.type == 1) {
-        try sender.send(2, @embedFile("./packet.txt"), .{});
-    }
-}
-
 pub fn main() !void {
+    // Debug -> gpa,
+    // ReleaseSafe -> c_allocator or page_allocator idk what even is page_allocator
+    // ReleaseFast, ReleaseSmall -> c_allocator,
+
     var gpa = std.heap.GeneralPurposeAllocator(.{ .verbose_log = true }){};
     defer _ = gpa.deinit();
 
     const allocator = gpa.allocator();
 
-    if (readArguments()) |args| {
-        const address = try getAddress();
-        // add colors and stuff make it fun
-        _ = args;
-        std.debug.print("Got server address: {s}:{d}\n", .{ address.host, address.port });
-        try connectToServer(allocator, address, on_packet);
-    }
-}
+    const args = readArguments() orelse return;
+    _ = args;
 
-const PacketSender = struct {
-    peer: *c.ENetPeer,
+    const address = try getAddress();
+    std.debug.print("Got server address: {s}:{d}\n", .{ address.host, address.port });
 
-    const SendError = error{
-        ENetPacketCreate,
-        ENetPeerSend,
-    };
+    var connection = try enet.connectToServer(allocator, address);
+    defer connection.deinit();
 
-    fn send(self: PacketSender, comptime packet_type: u32, comptime fmt: []const u8, args: anytype) SendError!void {
-        comptime var prefix: [4]u8 = undefined;
-        std.mem.writeInt(u32, &prefix, packet_type, .little);
-
-        // now enet allocs but there is c.ENET_PACKET_FLAG_NO_ALLOCATE flag we can use that
-        const enet_paket = c.enet_packet_create(null, std.fmt.count(prefix ++ fmt, args), c.ENET_PACKET_FLAG_RELIABLE);
-        if (enet_paket == null) {
-            return SendError.ENetPacketCreate; // we can use OutOfMemory
-        }
-        errdefer c.enet_packet_destroy(enet_paket);
-
-        _ = std.fmt.bufPrint(enet_paket.*.data[0..enet_paket.*.dataLength], prefix ++ fmt, args) catch unreachable;
-
-        if (c.enet_peer_send(self.peer, 0, enet_paket) != 0) {
-            return SendError.ENetPeerSend;
-        }
-
-        //std.debug.print("type: {d}\n", .{enet_paket.*.data[0..4]});
-        //std.debug.print("data: {s}\n", .{enet_paket.*.data[4..enet_paket.*.dataLength]});
-    }
-};
-
-const TranslatePacketError = error{
-    InvalidLength,
-};
-
-fn translatePacket(raw_packet: []const u8) TranslatePacketError!Packet {
-    if (raw_packet.len < 4) return TranslatePacketError.InvalidLength;
-    return .{
-        .type = std.mem.readInt(u32, raw_packet[0..4], .little),
-        .data = raw_packet[4..], // data itself should be translated too.
-    };
-}
-
-fn connectToServer(allocator: std.mem.Allocator, address: Address, comptime packet_callback: fn (allocator: std.mem.Allocator, packet: Packet, sender: PacketSender) anyerror!void) !void {
-    if (allocator.vtable == std.heap.c_allocator.vtable) {
-        if (c.enet_initialize() != 0) {
-            return error.EnetInitialize;
-        }
-    } else {
-        const Impl = struct {
-            var base_allocator: std.mem.Allocator = undefined;
-
-            fn malloc(size: usize) callconv(.C) ?*anyopaque {
-                const header = @sizeOf(usize);
-                const alignment = @alignOf(std.c.max_align_t);
-
-                const mem = base_allocator.alignedAlloc(u8, alignment, size + header) catch return null;
-                mem[0..header].* = @bitCast(size);
-
-                return mem.ptr + header;
-            }
-
-            fn free(memory: ?*anyopaque) callconv(.C) void {
-                const ptr = memory orelse return;
-
-                const header = @sizeOf(usize);
-                const alignment = @alignOf(std.c.max_align_t);
-
-                // this is unsafe if enet allocs data not using our allocator and frees it using ours
-                const original_ptr: *anyopaque = @ptrFromInt(@intFromPtr(ptr) - header);
-                const mem: [*]align(alignment) u8 = @ptrCast(@alignCast(original_ptr));
-
-                const size: usize = @bitCast(mem[0..header].*);
-                base_allocator.free(mem[0 .. size + header]);
-            }
-        };
-        // is it safe on multithreaded enviroments? we should add mutixes here just for safety base_allocator is a static variable
-        Impl.base_allocator = allocator;
-
-        if (c.enet_initialize_with_callbacks(c.ENET_VERSION, &.{
-            .malloc = Impl.malloc,
-            .free = Impl.free,
-            .no_memory = null, // we do not need it
-        }) != 0) {
-            return error.EnetInitialize;
+    while (try connection.next()) |packet| {
+        std.debug.print("got a packet({d}):\n{s}\n", .{ packet.type, packet.data });
+        if (packet.type == 1) {
+            try connection.sendPacket(2, @embedFile("./packet.txt"), .{});
         }
     }
-    defer c.enet_deinitialize();
 
-    const client = c.enet_host_create(null, 1, 2, 0, 0);
-    if (client == null) {
-        return error.ENetHostCreate;
-    }
-    defer c.enet_host_destroy(client);
-
-    var enet_address: c.ENetAddress = undefined;
-    client.*.usingNewPacket = 1;
-    client.*.checksum = c.enet_crc32;
-
-    if (c.enet_host_compress_with_range_coder(client) != 0) {
-        return error.ENetAddressCompress;
-    }
-
-    if (c.enet_address_set_host(&enet_address, address.host) != 0) {
-        return error.ENetAddressSetHost;
-    }
-    enet_address.port = address.port;
-
-    const peer = c.enet_host_connect(client, &enet_address, 2, 0);
-    if (peer == null) {
-        return error.ENetHostConnect;
-    }
-
-    const sender: PacketSender = .{
-        .peer = peer,
-    };
-
-    var event: c.ENetEvent = undefined;
-    loop: while (true) {
-        while (c.enet_host_service(client, &event, 0) > 0) {
-            switch (event.type) {
-                c.ENET_EVENT_TYPE_CONNECT => {},
-                c.ENET_EVENT_TYPE_RECEIVE => {
-                    // i have no i idea who collects memory and what allocs it
-                    // possible we need to free the packet or event
-                    const packet = translatePacket(event.packet.*.data[0..event.packet.*.dataLength]) catch |err| switch (err) {
-                        TranslatePacketError.InvalidLength => {
-                            std.debug.print("got packet with invalid length: {d}\n", .{event.packet.*.dataLength});
-                            break;
-                        },
-                        else => return err,
-                    };
-                    try packet_callback(allocator, packet, sender);
-                },
-                c.ENET_EVENT_TYPE_DISCONNECT => {
-                    std.debug.print("disconnect\n", .{}); // add disconect reason or just return error.Disconected
-                    break :loop;
-                },
-                else => {
-                    std.debug.print("unknown packet ev: {d}\n", .{event.type});
-                },
-            }
-        }
-        // can we sleep till we get an event?
-        std.time.sleep(std.time.ns_per_ms * 50);
-    }
+    std.debug.print("disconnected\n", .{});
 }
 
 fn readArguments() ?Arguments {
@@ -240,11 +86,11 @@ fn readArguments() ?Arguments {
     };
 }
 
-fn getAddress() !Address {
+fn getAddress() !enet.Address {
     // bad idk why: const url = "https://www.growtopiagame.com/growtopia/server_data.php"; // ip good port is not idk why
     // idk how to get the correct port
     return .{
         .host = "213.179.209.168",
-        .port = 17199, // this is bad port, good -> 17176
+        .port = 17176, // 17199 this is bad port, good -> 17176
     };
 }
