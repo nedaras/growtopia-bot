980ee2125d55e9800f92b9b4165626e214f957d4
nedaras <nedas.pranskunas@gmail.com>
2024-09-27 16:35:37 +0300

    Decoding server data.

--- src/main.zig
+++ src/main.zig
@@ -19,7 +19,12 @@ pub fn main() !void {
     //const args = readArguments() orelse return;
     //_ = args;
 
-    try getAddress(allocator);
+    const server_data = try getServerData(allocator);
+    std.debug.print("server: {s}\n", .{server_data.server()});
+    std.debug.print("login_url: {s}\n", .{server_data.loginUrl()});
+    std.debug.print("port: {d}\n", .{server_data.port});
+
+    //try getAddress(allocator);
     //std.debug.print("Got server address: {s}:{d}\n", .{ address.host, address.port });
 
     //var connection = try enet.connectToServer(allocator, address);
@@ -63,7 +68,25 @@ fn readArguments() ?Arguments {
     };
 }
 
-fn getAddress(allocator: std.mem.Allocator) !void { // i do not like how it allocated so many memory
+const ServerData = struct {
+    server_buf: [32]u8,
+    server_buf_len: u8,
+
+    login_url_buf: [32]u8,
+    login_url_len: u8,
+
+    port: u16,
+
+    fn server(server_data: *const ServerData) []const u8 {
+        return server_data.server_buf[0..server_data.server_buf_len];
+    }
+
+    fn loginUrl(server_data: *const ServerData) []const u8 {
+        return server_data.login_url_buf[0..server_data.login_url_len];
+    }
+};
+
+fn getServerData(allocator: std.mem.Allocator) !ServerData { // i do not like how it allocated so many memory
     var client = std.http.Client{ .allocator = allocator };
     defer client.deinit();
 
@@ -75,7 +98,11 @@ fn getAddress(allocator: std.mem.Allocator) !void { // i do not like how it allo
 
     var request = try client.open(.POST, uri, .{
         .server_header_buffer = &header_buffer,
-        .headers = .{ .user_agent = .{ .override = "UbiServices_SDK_2022.Release.9_PC64_ansi_static" }, .accept_encoding = .{ .override = "*/*" }, .content_type = .{ .override = "application/x-www-form-urlencoded" } },
+        .headers = .{
+            .user_agent = .{ .override = "UbiServices_SDK_2022.Release.9_PC64_ansi_static" },
+            .accept_encoding = .{ .override = "*/*" },
+            .content_type = .{ .override = "application/x-www-form-urlencoded" },
+        },
         .keep_alive = false,
     });
     defer request.deinit();
@@ -83,12 +110,56 @@ fn getAddress(allocator: std.mem.Allocator) !void { // i do not like how it allo
     request.transfer_encoding = .{ .content_length = body.len };
 
     try request.send();
-
     try request.writeAll(body);
 
     try request.finish();
     try request.wait();
 
     const len = try request.readAll(&response_buffer);
-    std.debug.print("response: \n{s}\n", .{response_buffer[0..len]});
+    const response = response_buffer[0..len];
+
+    var server_data: ServerData = .{
+        .server_buf = undefined,
+        .server_buf_len = 0,
+        .login_url_buf = undefined,
+        .login_url_len = 0,
+        .port = 0,
+    };
+
+    var iter = std.mem.tokenize(u8, response, "|\n");
+    while (iter.next()) |val| {
+        if (std.mem.eql(u8, val, "maint")) {
+            return error.UnderMaintenance;
+        }
+        if (std.mem.eql(u8, val, "port")) {
+            const port = iter.next() orelse continue;
+            server_data.port = try std.fmt.parseInt(u16, port, 10);
+        }
+        if (std.mem.eql(u8, val, "server")) {
+            const server = iter.next() orelse continue;
+
+            if (server.len > server_data.server_buf.len) {
+                return error.TooBig;
+            }
+
+            @memcpy(server_data.server_buf[0..server.len], server);
+            server_data.server_buf_len = @intCast(server.len);
+        }
+        if (std.mem.eql(u8, val, "loginurl")) {
+            const login_url = iter.next() orelse continue;
+
+            if (login_url.len > server_data.login_url_buf.len) {
+                return error.TooBig;
+            }
+
+            @memcpy(server_data.login_url_buf[0..login_url.len], login_url);
+            server_data.login_url_len = @intCast(login_url.len);
+        }
+    }
+
+    if (server_data.server_buf_len == 0 or server_data.login_url_len == 0 or server_data.port == 0) {
+        return error.MissingFields;
+    }
+
+    return server_data;
 }
